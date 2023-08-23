e3b22a5fdd7fabe0bf0b87faeb42a59e345d5d02
Griffin Proctor <GriffinProctor1@gmail.com>
2023-08-22 18:23:41 -0700

    123

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 import './less/index.less'
 
-// Your code goes here!
+window.onload = function () {
+    const heading = document.querySelector('h1')
+    heading.textContent = 'ADVENTURE AWAITS!'
+}
+
+let navLinks = document.querySelectorAll('.nav-link')
+navLinks.addEventListener('mouseover', (event) => {
+    event.target.style.backgroundColor = 'blue'
+})
+let button = document.querySelectorAll('.btn')
+
+button.addEventListener('click', (event) => { event.target.style.backgroundColor = 'red'; });
+
+document.body.addEventListener('click', evt => {
+    evt.target.classList.toggle('hi')
+})
+
+document.body.addEventListener('dblclick', evt => {
+    evt.target.innerHTML = 'oops'
+})
+window.addEventListener('keydown', evt => {
+if (evt.key == 1){
+    document.body.innerHTML = 'XOXO'
+}
+})
+document.body.addEventListener('mouseover', evt => {
+    const {clientX, clientY} = evt 
+})
+const destinations = document.querySelectorAll('.destination')
+for (let destination of destinations) {
+    destination.addEventListener('mouseover', evt => {
+        destination.style.fontWeight = 'bold'
+    })
+}
