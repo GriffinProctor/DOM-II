7e4dbf09d21c524aaad8961eb78ce15003cfde19
Griffin Proctor <GriffinProctor1@gmail.com>
2023-08-24 11:08:39 -0700

    complete

--- src/index.js
+++ src/index.js
@@ -3,8 +3,40 @@ import './less/index.less'
 window.onload = function () {
     const heading = document.querySelector('h1')
     heading.textContent = 'ADVENTURE AWAITS!'
-}
 
+
+
+    window.addEventListener('copy', () => {
+    navigator.clipboard.readText()
+    .then(text => {
+        heading.textContent += text
+    })
+
+})
+document.body.addEventListener('click', evt => {
+    evt.target.classList.toggle('mirror')
+})
+document.body.addEventListener('dblclick', evt => {
+    evt.target.outerHTML = 'oops'
+})
+window.addEventListener('keydown', evt => {
+    if (evt.key == 1){
+        document.body.innerHTML = 'XOXO'
+    }
+    })
+    document.body.addEventListener('mousemove', evt => {
+    const {clientX, clientY} = evt 
+    console.log(`mouse is at ${clientX}, ${clientY}`)
+})
+const destinations = document.querySelectorAll('.destination')
+for (let destination of destinations) {
+    destination.addEventListener('mouseenter', () => {
+        destination.style.fontWeight = 'bold'
+    })
+    destination.addEventListener('mouseleave', () => {
+        destination.style.fontWeight = 'initial'
+    })
+}
 let navLinks = document.querySelectorAll('.nav-link')
 navLinks.addEventListener('mouseover', (event) => {
     event.target.style.backgroundColor = 'blue'
@@ -13,24 +45,11 @@ let button = document.querySelectorAll('.btn')
 
 button.addEventListener('click', (event) => { event.target.style.backgroundColor = 'red'; });
 
-document.body.addEventListener('click', evt => {
-    evt.target.classList.toggle('hi')
-})
 
-document.body.addEventListener('dblclick', evt => {
-    evt.target.innerHTML = 'oops'
-})
-window.addEventListener('keydown', evt => {
-if (evt.key == 1){
-    document.body.innerHTML = 'XOXO'
 }
-})
-document.body.addEventListener('mouseover', evt => {
-    const {clientX, clientY} = evt 
-})
-const destinations = document.querySelectorAll('.destination')
-for (let destination of destinations) {
-    destination.addEventListener('mouseover', evt => {
-        destination.style.fontWeight = 'bold'
-    })
-}
+
+
+
+
+
+
